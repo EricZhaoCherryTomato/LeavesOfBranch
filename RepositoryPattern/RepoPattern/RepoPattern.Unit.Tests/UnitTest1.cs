d7917418d70f57828d52b1006ac9e3382bcb9911
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-13 11:39:43 +1000

    NUnit test framwork with Moq

---
+++ RepositoryPattern/RepoPattern/RepoPattern.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace RepoPattern.Unit.Tests
+{
+    [TestFixture]
+    public class PersonSeviceTests
+    {
+        [Test]
+        public void ShouldBeAbleToCallPersonServiceAndGetPerson()
+        {
+            var expected = new Person { Id = 1, FirstName = "John", LastName = "Doe"};
+            var personReposityMock = new Mock<IPersonRepository>();
+            personReposityMock.Setup(pr => pr.GetPerson(1)).Returns(new Person { Id = 1, FirstName = "John", LastName = "Doe" });
+            var personService = new PersonService(personReposityMock.Object);
+            var actual = personService.GetPerson(expected.Id);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+        }
+    }
+}
