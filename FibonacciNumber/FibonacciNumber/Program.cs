d5eb918b28f07d17e75e3ec969216942670d2cc3
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-20 13:04:36 +1000

    Iterate approach to generate fibonacci

---
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumber
+{
+    class Program
+    {
+        static int FibonacciSeries(int n)
+        {
+            int firstNum = 0;
+            int secondNum = 1;
+            int result = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = result;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
