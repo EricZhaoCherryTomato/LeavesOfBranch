69f3cb4ad859e89a5d64562d336992bb5e946ef5
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-23 11:21:50 +1000

    Fast finder

--- FibonacciNumber/FibonacciNumber/Program.cs
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +10,41 @@ namespace FibonacciNumber
 {
     class Program
     {
-        static int NthFibonacciSeries(int n)
+
+        // Fast doubling algorithm
+        private static BigInteger Fibonacci(int n)
+        {
+            BigInteger a = BigInteger.Zero;
+            BigInteger b = BigInteger.One;
+            for (int i = 31; i >= 0; i--)
+            {
+                BigInteger d = a * (b * 2 - a);
+                BigInteger e = a * a + b * b;
+                a = d;
+                b = e;
+                if ((((uint)n >> i) & 1) != 0)
+                {
+                    BigInteger c = a + b;
+                    a = b;
+                    b = c;
+                }
+            }
+            return a;
+        }
+
+        static long NthFibonacciSeries(long n)
         {
             if (n == 0 || n == 1)
             {
                 return n;
             }
             return NthFibonacciSeries(n-1) + NthFibonacciSeries(n-2);
+        }
+        static BigInteger NegativeNthFibonacciSeries(BigInteger n, long number)
+        {
+            BigInteger sign;
+            BigInteger.TryParse(Math.Pow(-1, Math.Abs(number) + 1).ToString(CultureInfo.InvariantCulture), out sign);
+            return  sign * n;
         }
 
         //static int NegativeNthFibonacciSeries(int n)
@@ -41,13 +71,14 @@ namespace FibonacciNumber
         }
         static void Main(string[] args)
         {
-            Console.Write(-1-1);
             Console.Write("Enter the nth number of the Fibonacci Series: ");
-            int length = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < length; i++)
-            {
-                Console.Write("{0} ", FibonacciSeries(i));
-            }
+            //int length = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < length; i++)
+            //{
+            //    Console.Write("{0} ", FibonacciSeries(i));
+            //}
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0} ", number < 0 ? NegativeNthFibonacciSeries(Fibonacci(Math.Abs(number)), number) : Fibonacci(number));
             Console.ReadKey();
         }
     }
