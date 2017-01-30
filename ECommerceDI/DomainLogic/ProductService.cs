34a85b2dd2aba0476a145dd4361de8b46d02d63b
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-30 15:54:37 +1000

    Do it wrong

---
+++ ECommerceDI/DomainLogic/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SqlDataAccess;
+
+namespace DomainLogic
+{
+    public class ProductService
+    {
+        private readonly CommerceObjectContext _objectContext;
+
+        public ProductService()
+        {
+            _objectContext = new CommerceObjectContext();
+        }
+
+        public IEnumerable<Product> GetFeaturedProducts(
+            bool isCustomerPreferred)
+        {
+            var discount = isCustomerPreferred ? .95m : 1;
+            var products = (from p in _objectContext
+                                .Products
+                            where p.IsFeatured
+                            select p).AsEnumerable();
+            return from p in products
+                   select new Product
+                          {
+                              ProductId = p.ProductId,
+                              Name = p.Name,
+                              Description = p.Description,
+                              IsFeatured = p.IsFeatured,
+                              UnitPrice = p.UnitPrice * discount
+                          };
+        }
+    }
+}
---
+++ ECommerceDI/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using DomainLogic;
+using SqlDataAccess;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var isPreferredCustomer =
+                User.IsInRole("PreferredCustomer");
+            var service = new ProductService();
+            var products =
+                service.GetFeaturedProducts(isPreferredCustomer);
+            ViewData["Products"] = products;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
